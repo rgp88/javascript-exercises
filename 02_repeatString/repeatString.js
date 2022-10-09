2310795b824d814303ae5fcc7fb90e2fb16dd3ef
rgp88 <ragiphasani@gmail.com>
2022-10-09 23:33:30 +0200

    2 tasks completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+let b ;
+const repeatString = function(text,num) {
+    if(num == 0) {
+        b = "";
+    } else if(num <0) {
+        b = "ERROR" 
+    } else {
+    b = text;    
+    for (let i=1;i<num;i++) {
+        b += text;
+    }
+}
+    return b;
 
 };
 
