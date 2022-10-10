73e4444ef3a5f4ac5efedee3c7504af6ad9dd471
rgp88 <ragiphasani@gmail.com>
2022-10-11 00:31:21 +0200

    completed removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+string = string.split("");
+let reverse = [];
+for (i=string.length-1;i>=0;i--) {
+    reverse.push(string[i]);
+}
+return reverse.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,31 @@
-const removeFromArray = function() {
+const removeFromArray = function(a,b,c,d,e) {
+    if (b === undefined || a.indexOf(b)<0) {
+        a;
+    } else {
+        a.splice(a.indexOf(b),1);
+    }
 
+    if (c === undefined || a.indexOf(c)<0) {
+        a;
+    } else {
+        a.splice(a.indexOf(c),1);
+    }
+
+    if (d === undefined || a.indexOf(d)<0) {
+        a;
+    } else {
+        a.splice(a.indexOf(d),1);
+    }
+
+    if (e === undefined || a.indexOf(e)<0) {
+        a;
+    } else {
+        a.splice(a.indexOf(e),1);
+    }
+    return a;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
